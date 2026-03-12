cb37e3dc7b32321ea0d5b1af2a934ff5a1ed5a9a
Jonathan Rom <yonirom@gmail.com>
2026-03-12 17:38:05 +0000

    fix connectin closing, lint

--- main.py
+++ main.py
@@ -24,25 +24,31 @@ SETTINGS = Settings()
 
 # 1. Define the session handler (the "server-side" session)
 class SSHCertServerSession(asyncssh.SSHServerSession):
-
     def __init__(self, conn: asyncssh.SSHServerConnection) -> None:
         super().__init__()
         self._conn = conn
-        self._chan: asyncssh.SSHServerChannel = None
+        self._chan: Optional[asyncssh.SSHServerChannel] = None
 
     def connection_made(self, chan: asyncssh.SSHServerChannel):
         self._chan = chan
 
     def session_started(self) -> None:
+        if self._chan is None:
+            return
         self._chan.write(SETTINGS.post_auth_banner)
         self._chan.write_eof()
         self._chan.close()
+        asyncio.create_task(self._close_connection())
+
+    async def _close_connection(self):
+        if self._chan is not None:
+            await self._chan.wait_closed()
         self._conn.close()
 
     def shell_requested(self):
         # Allow shell requests
         return True
-        
+
 
 class SSHCertSignerServer(asyncssh.SSHServer):
     def __init__(self, app_config: config_module.AppConfig):
@@ -103,8 +109,7 @@ class SSHCertSignerServer(asyncssh.SSHServer):
         self._conn.set_extra_info(username=username)
 
         logger.info(
-            f"Auth attempt for user: {
-                username} with public key type: {key.algorithm}"
+            f"Auth attempt for user: {username} with public key type: {key.algorithm}"
         )
 
         if self.user_found:
@@ -114,10 +119,7 @@ class SSHCertSignerServer(asyncssh.SSHServer):
                 logger.warning(f"Invalid key in config for user {username}")
                 return False
             if key == user_key:
-                logger.info(
-                    f"publickey authentication succeeded for {
-                        username}"
-                )
+                logger.info(f"publickey authentication succeeded for {username}")
                 return True
         return False
 
@@ -128,17 +130,25 @@ class SSHCertSignerServer(asyncssh.SSHServer):
             logger.info("Connection closed.")
 
 
-def sign_certificate(user_config: config_module.UserConfig) -> Tuple[SSHKey, SSHOpenSSHCertificate]:
+def sign_certificate(
+    user_config: config_module.UserConfig,
+) -> Tuple[SSHKey, SSHOpenSSHCertificate]:
     new_user_key: SSHKey = asyncssh.generate_private_key("ssh-ed25519")
     ca: SSHKey = asyncssh.read_private_key(SETTINGS.ca_private_key_file)
     principals = user_config.principals or user_config.username
-    user_certificate: SSHOpenSSHCertificate= ca.generate_user_certificate(new_user_key, key_id="meepmeep", principals=principals, valid_after=datetime.now(), valid_before=user_config.valid_for)
+    user_certificate: SSHOpenSSHCertificate = ca.generate_user_certificate(
+        new_user_key,
+        key_id="meepmeep",
+        principals=principals,
+        valid_after=datetime.now(),
+        valid_before=user_config.valid_for,
+    )
     new_user_key.set_comment(SETTINGS.installation_id)
 
     return new_user_key, user_certificate
 
-class EntryPoint():
 
+class EntryPoint:
     def __init__(self) -> None:
         self.load_config()
 
@@ -148,33 +158,43 @@ class EntryPoint():
         )
         logger.info("Configuration loaded successfully")
 
-
     async def reload_config(self):
         self.app_config: config_module.AppConfig = config_module.AppConfig.load_config(
             SETTINGS.config_file
         )
         logger.info("Configuration reloaded successfully")
-        
 
     async def monitor_conf_dir(self, directory: Path):
-        async for _ in awatch(directory, watch_filter=ConfigFilter(), recursive=False, force_polling=False):
+        async for _ in awatch(
+            directory, watch_filter=ConfigFilter(), recursive=False, force_polling=False
+        ):
             await self.reload_config()
 
     async def start_server(self):
         """Start the SSH server."""
         options: asyncssh.SSHServerConnectionOptions = SSHServerConnectionOptions(
-            server_factory=lambda: SSHCertSignerServer(app_config=self.app_config), 
+            server_factory=lambda: SSHCertSignerServer(app_config=self.app_config),
             server_version=SETTINGS.server_version,
             server_host_keys=SETTINGS.host_private_key_file,
             agent_forwarding=True,
             connect_timeout="5s",
-            login_timeout="10s")
-        await asyncssh.listen(str(SETTINGS.listen_address), SETTINGS.listen_port, reuse_address=True, options=options)
+            login_timeout="10s",
+        )
+        await asyncssh.listen(
+            str(SETTINGS.listen_address),
+            SETTINGS.listen_port,
+            reuse_address=True,
+            options=options,
+        )
 
 
 class ConfigFilter(DefaultFilter):
     def __call__(self, change: Change, path: str) -> bool:
-        return ( super().__call__(change, path) and path.endswith("config.yaml") and change in (Change.added, Change.modified))
+        return (
+            super().__call__(change, path)
+            and path.endswith("config.yaml")
+            and change in (Change.added, Change.modified)
+        )
 
 
 async def main():
@@ -188,8 +208,6 @@ async def main():
 
 
 if __name__ == "__main__":
-
-
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
